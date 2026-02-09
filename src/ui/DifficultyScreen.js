482b2acc83b8eca9b8de8a9dd89b6dceabe5a6d8
LAKSHAYJAIN19 <72019776+LAKSHAYJAIN19@users.noreply.github.com>
2026-02-09 15:57:38 +0530

    Update DifficultyScreen.js

--- src/ui/DifficultyScreen.js
+++ src/ui/DifficultyScreen.js
@@ -8,49 +8,6 @@ export function DifficultyScreen(container) {
         return '/images/android_size/multibga.png';
     })();
 
-//     container.innerHTML = `
-//     <div class="difficulty-bg" style="background-image: url('${bgImage}')">
-//       <div class="difficulty-bg">
-//         <div class="difficulty-overlay">
-//
-//           <h2 class="difficulty-title">Difficulty Level ?</h2>
-//
-//           <div class="difficulty-options">
-//             <button class="difficulty-btn">
-//               <strong>Easy</strong>
-//               <span>Wind level (0–10 m/s)</span>
-//             </button>
-//             <button class="difficulty-btn">
-//               <strong>Medium</strong>
-//               <span>Wind level (15–30 m/s)</span>
-//             </button>
-//             <button class="difficulty-btn">
-//               <strong>Hard</strong>
-//               <span>Wind level (35–45 m/s)</span>
-//             </button>
-//             <button class="difficulty-btn">
-//               <strong>Expert</strong>
-//               <span>Wind level (50–60 m/s)</span>
-//             </button>
-//           </div>
-//           <div class="buttons">
-//             <button id="backToMainBtn" class="primary">
-//               Back to main screen
-//             </button>
-//           </div>
-//
-//         </div>
-//       </div>
-//     </div>
-//   `;
-//
-//     const backBtn = container.querySelector('#backToMainBtn');
-//     backBtn.addEventListener('click', () => {
-//         container.dispatchEvent(new CustomEvent('navigateMain'));
-//     });
-//
-// }
-
     container.innerHTML = `
     <div class="difficulty-screen">
       <div 
@@ -65,21 +22,25 @@ export function DifficultyScreen(container) {
           <button class="difficulty-btn" data-level="easy">
             <strong>Easy</strong>
             <span>Wind level (0–10 m/s)</span>
+            <span>Target visibilty (100%)</span>
           </button>
 
           <button class="difficulty-btn" data-level="medium">
             <strong>Medium</strong>
             <span>Wind level (15–30 m/s)</span>
+            <span>Target visibilty (90%)</span>
           </button>
 
           <button class="difficulty-btn" data-level="hard">
             <strong>Hard</strong>
             <span>Wind level (35–45 m/s)</span>
+            <span>Target visibilty (70%)</span>
           </button>
 
           <button class="difficulty-btn" data-level="expert">
             <strong>Expert</strong>
             <span>Wind level (50–60 m/s)</span>
+            <span>Target visibilty (50%)</span>
           </button>
         </div>
 
@@ -92,6 +53,19 @@ export function DifficultyScreen(container) {
     </div>
   `
 
+  //difficulty click handlers
+    container.querySelectorAll('.difficulty-btn').forEach(btn => {
+        btn.addEventListener('click', () => {
+            const level = btn.dataset.level;
+            container.dispatchEvent(
+                new CustomEvent('navigateGame', {
+                    detail: { level }
+                })
+            );
+        });
+    });
+
+  //back to main screen
     container
         .querySelector('#backToMainBtn')
         .addEventListener('click', () => {
